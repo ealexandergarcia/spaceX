2600eee8f4de69f7fd1ca45655fcd8143f42b71a
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-26 09:04:35 -0500

    feat: :sparkles: agrega funcionalidad de carga y visualización de datos de cohetes con filtros y paginación

---
+++ js/main.js
@@ -0,0 +1,35 @@
+import { load } from "./Components/load.js";
+import { paginationRockets } from "./Components/pagination.js";
+import { defecto, masa } from "./helper/filtros.js";
+import { getAllRockets } from "./module/rocket.js";
+
+let data3 = await getAllRockets(defecto);
+console.log(data3);
+
+let data = await getAllRockets(masa);
+console.log(data);
+
+
+
+
+let footerSelect = async(e, id)=>{
+    e.preventDefault();
+    await load();
+    let li = document.querySelectorAll(".footer ul li")
+    for(let val of li){
+        let [a] = val.children
+        a.classList.remove('select');
+    }
+    let [a] = id.children
+    a.classList.add('select');   
+}
+let rocket = document.querySelector("#rocket")
+rocket.addEventListener("click", async(e)=>{
+    await footerSelect(e, rocket)
+    let paginacion = document.querySelector("#paginacion");
+    paginacion.innerHTML = ""
+    paginacion.append(await paginationRockets())
+} )
+
+
+rocket.click();

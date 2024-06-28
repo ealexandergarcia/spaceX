d100064c0202ec367f97c9047a3c5612bf307ded
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-28 11:12:37 -0500

    feat: ✨ Creacion de funcion para la conexion con el api de capsules

--- js/module/rocket.js
+++ js/module/rocket.js
@@ -11,4 +11,18 @@ export const getAllRockets = async (optio)=>{
     let {docs:mass}= await res.json();
     return mass;
 }
+export const getAllCapsules = async (optio)=>{
+    const url ="https://api.spacexdata.com/v4/capsules/query"
+    let config = {
+        headers:{
+            "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(optio)
+    }
+    let res = await fetch(url, config);
+    let data = await res.json();
+    console.log(data);
+    return data;
+}
 

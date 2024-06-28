993cd0f4e78df68fd1d97ba6eb8f071ad13c460c
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-27 23:05:24 -0500

    feat: :sparkles: agregar filtros auxiliares para datos de cohetes

--- js/helper/filtros.js
+++ js/helper/filtros.js
@@ -17,6 +17,63 @@ export const masa = {
         },
         "sort":{
             "mass.kg": "desc"
+        },
+        "limit": 1
+    }
+}
+
+export const carga = {
+    "options": {
+        "select": {
+            "name": 1,
+            "payload_weights": 1
+        },
+        "sort":{
+            "payload_weights.kg": "desc"
+        },
+    }
+}
+
+export const RocketHeightTotal = {
+    "options": {
+        "select": {
+            "height": 1
+        },
+        "sort": {
+            "height.meters": "desc"
+        }
+    }
+}
+
+export const RocketDiameterTotal = {
+    "options": {
+        "select": {
+            "diameter": 1
+        },
+        "sort": {
+            "diameter.meters": "desc"
+        }
+    }
+}
+
+export const SecondStageDiameterTotal = {
+    "options": {
+        "select": {
+            "second_stage.payloads.composite_fairing.diameter": 1
+        },
+        "sort": {
+            "second_stage.payloads.composite_fairing.diameter": "desc"
+        }
+    }
+}
+
+export const SecondStageHeightTotal = {
+    "options": {
+        "select": {
+            "second_stage.payloads.composite_fairing.height": 1
+        },
+        "sort": {
+            "second_stage.payloads.composite_fairing.height": "desc"
         }
     }
 }

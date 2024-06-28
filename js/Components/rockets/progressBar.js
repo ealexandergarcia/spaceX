789d0746568cdac850d4c0ee939f726a51d8aeb0
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-27 23:26:49 -0500

    feat: :sparkles: agregar progreso de peso de carga útil
    Se ha agregado una nueva función progressPayloadWeights que muestra el progreso del peso de carga útil de un cohete en la sección de información. La función utiliza los helpers de filtrado carga para obtener la lista de pesos de carga útil y luego crea elementos HTML para mostrar el progreso de cada peso de carga útil.

--- js/Components/rockets/progressBar.js
+++ js/Components/rockets/progressBar.js
@@ -40,3 +40,51 @@ export const progressRocketWeight = async (rocket) => {
   loadDiv.classList.replace('load', 'information__2');
 };
 
+export const progressPayloadWeights = async (Rockets) => {
+  let docs = await getAllRockets(carga);
+  let data = []
+  docs.forEach(val => data.push(...val.payload_weights))
+  data.sort((a, b) => b.kg - a.kg)
+  let [maxPayloadWeightsRocket] = data;
+  let { kg } = maxPayloadWeightsRocket
+
+  let conterDiv = [];
+
+  // Paso 3: Iterar sobre la lista de pesos de carga útil
+  Rockets.payload_weights.forEach((val) => {
+    // Paso 4: Crear el contenedor de información
+    let divInformationContainer = document.createElement("div");
+    divInformationContainer.classList.add("information__container");
+
+    // Paso 5: Crear la etiqueta y el progreso
+    let divFirst = document.createElement("div");
+    let labelFist = document.createElement("label");
+    labelFist.textContent = `${val.name} :`;
+
+    let ProgressFist = document.createElement("progress");
+    ProgressFist.max = kg;
+    ProgressFist.value = `${val.kg}`;
+    ProgressFist.textContent = `${val.kg}%`;
+
+    // Paso 6: Crear el span con la información de peso
+    let divLast = document.createElement("div");
+    let spanLast = document.createElement("span");
+    let numKg = new Intl.NumberFormat('cop').format(val.kg);
+    let numLb = new Intl.NumberFormat('cop').format(val.lb);
+    spanLast.innerHTML = `${numKg} kg <br> ${numLb} lb`;
+
+    // Paso 7: Agregar los elementos al contenedor
+    divFirst.append(labelFist);
+    divFirst.append(ProgressFist);
+    divLast.append(spanLast);
+    divInformationContainer.append(divFirst);
+    divInformationContainer.append(divLast);
+    conterDiv.push(divInformationContainer);
+  });
+
+  // Paso 8: Agregar los contenedores al elemento information__2
+  let section__information__1 = document.querySelector(".section__information__1");
+  let information__2 = section__information__1.querySelector(".information__2");
+  information__2.append(...conterDiv);
+};
+

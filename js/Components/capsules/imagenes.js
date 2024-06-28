c395146b02e3b46414b3ed5a066adf4a6607aea3
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-28 14:48:44 -0500

    feat: ✨ Generacion de imagen de la capsula

---
+++ js/Components/capsules/imagenes.js
@@ -0,0 +1,25 @@
+export const imageCapsule = async (capsule) => {    
+    let {launches:[{links:{patch:{small}}}]} = capsule;
+    console.log(small);
+
+    const sectionImage = document.querySelector("#section__image");
+    sectionImage.setAttribute("style","height: 260px;")
+    const divs = [];
+
+
+    const div = document.createElement("div");
+    div.classList.add("carousel__item");
+    // div.style.add("width: 250px;")
+
+    const img = document.createElement("img");
+    img.setAttribute("src", small);
+    img.setAttribute("referrerpolicy", "no-referrer");
+    img.setAttribute("style","width: 250px;height: 250px;")
+
+    div.appendChild(img);
+    divs.push(div);
+    const loadElement = sectionImage.querySelector("div.load");
+
+    loadElement.parentNode.replaceChild(div, loadElement);
+    // sectionImage.appendChild(div);
+};

458434c8ee7a49529603f9372a46c83230da0ddd
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-29 18:43:12 -0500

    feat: :sparkles: Funcion para colocar imagenes en company

---
+++ js/Components/company/imagenes.js
@@ -0,0 +1,22 @@
+export const imageCompany = async () => {    
+    const sectionImage = document.querySelector("#section__image");
+    sectionImage.setAttribute("style","height: 260px; display:block;")
+    const divs = [];
+
+
+    const div = document.createElement("div");
+    div.classList.add("carousel__item");
+    // div.style.add("width: 250px;")
+
+    const img = document.createElement("img");
+    img.setAttribute("src", "../storage/img/elon.webp");
+    img.setAttribute("referrerpolicy", "no-referrer");
+    img.setAttribute("style","width: 250px;height: 250px;")
+
+    div.appendChild(img);
+    divs.push(div);
+    const loadElement = sectionImage.querySelector("div.load");
+
+    loadElement.parentNode.replaceChild(div, loadElement);
+    // sectionImage.appendChild(div);
+};

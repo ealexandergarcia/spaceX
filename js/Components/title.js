8cfdf488a038e1c2e0b7414fb0266233271dabfe
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-29 18:42:40 -0500

    feat: :sparkles: Funcion titulo para colocar imagenes en eñ header

--- js/Components/title.js
+++ js/Components/title.js
@@ -3,3 +3,13 @@ export const title = async(name)=>{
     header__title.innerHTML = "";
     header__title.textContent = name;
 }
+export const title2 = async() => {
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = "";  // Limpiar el contenido existente
+
+    let img = document.createElement("img");  // Crear un nuevo elemento img
+    img.src = "../storage/img/icons/SpaceX-Logo.svg";  // Establecer la URL de la imagen
+    img.alt = "Header Image";  // Establecer un texto alternativo para la imagen
+
+    header__title.appendChild(img);  // Agregar la imagen al elemento
+}
